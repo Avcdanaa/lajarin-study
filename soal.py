e4f2fe8e4c19c4ff6ac8f5dd5b57b35a73b6bc8c
extrayiban <mzidanabdulkadir@gmail.com>
2025-11-05 14:59:58 +0800

    soal2

--- soal.py
+++ soal.py
@@ -8,4 +8,12 @@ string
 char
 
 masing masing di beri komen tipe datanya apa    
+
+
+
+2. Percabangan
+buat sebuah variabel bernama umur dengan nilai sembarang terus lakukan kondisi
+jika umur lebih dari 50 maka print di layar terminal "TUA WOY"
+sementara jika lebih dari 17 dan dibawah 50 print di layar "dewasa tapi korupsi"
+dan jika tidak semua itu maka print di layar "aku kecil dan aku masih bahagia"
 """
